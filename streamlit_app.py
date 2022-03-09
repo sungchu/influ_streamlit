2a1155506895c55d52a63bf5ea70b6b4f34540d9
sung-chu <songzhu1030@gmail.com>
2022-03-09 15:38:25 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,8 +6,44 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 import pickle
-from load_css import local_css
-local_css("style.css")
+st.markdown(
+    """
+<style>
+.reportview-container .markdown-text-container {
+    font-family: monospace;
+}
+.sidebar .sidebar-content {
+    background-image: linear-gradient(#2e7bcf,#2e7bcf);
+    color: white;
+}
+.Widget>label {
+    color: white;
+    font-family: monospace;
+}
+[class^="st-b"]  {
+    color: white;
+    font-family: monospace;
+}
+.st-bb {
+    background-color: transparent;
+}
+.st-at {
+    background-color: #0c0080;
+}
+footer {
+    font-family: monospace;
+}
+.reportview-container .main footer, .reportview-container .main footer a {
+    color: #0c0080;
+}
+header .decoration {
+    background-image: none;
+}
+
+</style>
+""",
+    unsafe_allow_html=True,
+)
 
 st.set_page_config(layout="wide")
 st.title('流感分級系統')
